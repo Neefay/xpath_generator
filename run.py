0b242dc0befd74160586fa3a0ce33c48c4d5d445
Neefay <notnife@hotmail.com>
2019-02-07 23:52:00 -0200

    Initial

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from modules.gen_xpath import main as gen_xpath
 
 ### READS DEFAULT JSON OR DRAG-DROPPED FILE AND RUN SCRIPT
 
-config = ("config.json" if (len(sys.argv) == 1) else sys.argv[1])
+config = ("config_example.json" if (len(sys.argv) == 1) else sys.argv[1])
 gen_xpath(config)
 
 input("Press any key to continue...")
